6f76e5dcfcbe06cd0cea55a2db37c40a09ae5eaf
kentanvictor <2030457926@qq.com>
2017-08-30 10:09:25 +0800

    添加數據

--- app/src/main/java/com/example/dell/platform_doctor/model/AddData.java
+++ app/src/main/java/com/example/dell/platform_doctor/model/AddData.java
@@ -1,11 +1,20 @@
 package com.example.dell.platform_doctor.model;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.dell.platform_doctor.gson.Person;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.bmob.v3.BmobBatch;
+import cn.bmob.v3.BmobObject;
+import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.datatype.BatchResult;
 import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.QueryListListener;
 import cn.bmob.v3.listener.SaveListener;
 
 /*
@@ -27,6 +36,55 @@ public class AddData {
                     showToast(context, "添加数据成功，返回objectId为：" + objectId);
                 } else {
                     showToast(context, "创建数据失败：" + e.getMessage());
+                }
+            }
+        });
+    }
+
+    //批量添加數據
+    public void addPlentyData(final Context context) {
+        List<BmobObject> persons = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Person person = new Person();
+            person.setName("張三");
+            person.setAddress("北京海淀");
+            persons.add(person);
+        }
+        //v.3.5.0之後開始提供的批量添加數據方法
+        new BmobBatch().insertBatch(persons).doBatch(new QueryListListener<BatchResult>() {
+            @Override
+            public void done(List<BatchResult> list, BmobException e) {
+                if (e == null) {
+                    for (int i = 0; i < list.size(); i++) {
+                        BatchResult result = list.get(i);
+                        BmobException ex = result.getError();
+                        if (ex == null) {
+                            showToast(context, "第" + (i + 1) + "個數據批量添加成功:" + result.getCreatedAt() + "," + result.getObjectId() + "," + result.getUpdatedAt());
+                        } else {
+                            showToast(context, "第" + (i + 1) + "个数据批量添加失败：" + ex.getMessage() + "," + ex.getErrorCode());
+                        }
+                    }
+                } else {
+                    Log.i("bmob", "失败：" + e.getMessage() + "," + e.getErrorCode());
+                }
+            }
+        });
+    }
+
+    //用戶註冊
+    public void setUser(final Context context) {
+        BmobUser user = new BmobUser();
+        user.setUsername("sendi");
+        user.setPassword("123456");
+        user.setEmail("sendi@gmail.com");
+        //不能用save方法進行註冊
+        user.signUp(new SaveListener<Person>() {
+            @Override
+            public void done(Person p1, BmobException e) {
+                if (e == null) {
+                    showToast(context,"注册成功:" + p1.toString());
+                } else {
+                    showToast(context,"done:" + e);
                 }
             }
         });
