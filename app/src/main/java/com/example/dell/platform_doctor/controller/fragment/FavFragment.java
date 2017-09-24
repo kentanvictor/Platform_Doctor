faf87c55de80b879edacb4bb2afbfed86cb8958f
kentanvictor <2030457926@qq.com>
2017-09-24 16:51:54 +0800

    FloatingSearchView(bug)

--- app/src/main/java/com/example/dell/platform_doctor/controller/fragment/FavFragment.java
+++ app/src/main/java/com/example/dell/platform_doctor/controller/fragment/FavFragment.java
@@ -43,7 +43,7 @@ public class FavFragment extends Fragment {
     private void initViewData() {
 
         List<ContentFragment> fragments = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5;i++) {
             fragments.add(ContentFragment.newInstance(i));
         }
         mTabLayout.setTabMode(TabLayout.MODE_FIXED);
