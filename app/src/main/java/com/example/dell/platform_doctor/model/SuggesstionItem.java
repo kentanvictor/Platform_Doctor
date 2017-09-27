d6c8c2635bf396b0e8028fb53a28db89593ea6c8
kentanvictor <2030457926@qq.com>
2017-09-27 17:54:10 +0800

    FloatingSearchView(bug)

--- app/src/main/java/com/example/dell/platform_doctor/model/SuggesstionItem.java
+++ app/src/main/java/com/example/dell/platform_doctor/model/SuggesstionItem.java
@@ -9,18 +9,46 @@ import com.arlib.floatingsearchview.suggestions.model.SearchSuggestion;
  * Created by JohnnyTan on 2017/9/27.
  */
 
-public class SuggesstionItem implements SearchSuggestion{
+public class SuggesstionItem implements SearchSuggestion {
     private String title;
     private boolean isHistory = false;
-    public SuggesstionItem(String title)
-    {
+
+    public SuggesstionItem(String title) {
         this.title = title;
     }
+
+    //从parcel中读取存储数据
+    public SuggesstionItem(Parcel source) {
+        title = source.readString();
+        //==1 : true ; ==0 : false
+        isHistory = source.readInt() != 0;
+    }
+
+    public void setHistory(boolean history) {
+        isHistory = history;
+    }
+
+    public boolean isHistory() {
+        return isHistory;
+    }
+
     @Override
     //getTitle
     public String getBody() {
         return title;
     }
+
+    public static final Creator<SuggesstionItem> CREATOR = new Creator<SuggesstionItem>() {
+        @Override
+        public SuggesstionItem createFromParcel(Parcel source) {
+            return new SuggesstionItem(source);
+        }
+
+        @Override
+        public SuggesstionItem[] newArray(int size) {
+            return new SuggesstionItem[size];
+        }
+    };
 
     @Override
     public int describeContents() {
